a11e59314b64e26534c80fae8a46d688a40a8426
rp-codelabs <raajponnam@gmail.com>
2017-05-31 15:36:37 +0100

    [Cleaning assemblies]

--- CarPark/CarPark/Properties/AssemblyInfo.cs
+++ CarPark/CarPark/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CarPark")]
+[assembly: AssemblyTitle("CarPark Kata")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CarPark")]
+[assembly: AssemblyProduct("CarPark Kata")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
